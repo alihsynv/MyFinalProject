acb7c8f5f9a0abab1def719e6d26d15f2bfd02b5
alihsynv <ali.hsynov@gmail.com>
2023-03-27 15:24:46 +0400

    Business codes writted

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
